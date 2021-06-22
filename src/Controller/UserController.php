b317d0f9c921a0d5d3a20f8d6bcdc0bca78fe0b2
christopheLabDev <christophelaborde@protonmail.com>
2021-06-22 16:33:24 +0200

    crud et mot de passe

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @Route("/user")
@@ -28,13 +29,21 @@ class UserController extends AbstractController
     /**
      * @Route("/new", name="user_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+                        // encode the plain password
+        $user->setPassword(
+        $passwordEncoder->encodePassword(
+        $user,
+        $form->get('plainPassword')->getData()
+        )
+        );
+        
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
@@ -61,12 +70,19 @@ class UserController extends AbstractController
     /**
      * @Route("/{id}/edit", name="user_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, User $user): Response
+    public function edit(Request $request, User $user, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+        $user->setPassword(
+        $passwordEncoder->encodePassword(
+        $user,
+        $form->get('plainPassword')->getData()
+    )
+    );
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('user_index');
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,11 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
@@ -13,10 +18,33 @@ class UserType extends AbstractType
     {
         $builder
             ->add('login')
-            ->add('roles')
-            ->add('password')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'administrateur' => 'ROLE_ADMIN',
+                    'cuistot' => 'ROLE_USER',
+                    'modérateur' => 'ROLE_MODERATEUR'
+                ],
+                'multiple' => true,
+                'expanded' => false
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'constraints' => [
+                new NotBlank([
+                'message' => 'Entrez un mot de passe, s\'il vous plait',
+                ]),
+                new Length([
+                'min' => 6,
+                'minMessage' => 'Votre mot de passe devrait avoir au moins {{ limit }} caractères',
+                // max length allowed by Symfony for security reasons
+                'max' => 4096,
+                ]),
+                ]
+                ])
+                
+            
             ->add('avatar')
-            ->add('email')
+            ->add('email', EmailType::class)
         ;
     }
 
