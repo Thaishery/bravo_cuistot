11a93426f61e7d5d4433c29c0e920a64e3edb34d
jordancarmont <biziboy91@gmail.com>
2021-06-22 11:45:54 +0200

    Mogif RegistrationFormType, Ajout règle Regex

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,42 +6,90 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\File;
+
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('login')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
+         $builder
+             ->add('login', TextType::class,[
+                 'constraints' => [
+                     new Assert\Regex([
+                         'pattern' => '/^[a-zA-Z0-9]*$/',
+                         'message' => 'Votre login ne peut comporter que des caractéres alphanumérique.'
+                     ])
+                 ],
+                 'label_attr' => ['class' => 'form-label'],
+                 'required' => true,
+             ])
+
+             ->add('agreeTerms', CheckboxType::class, [
+                 'mapped' => false,
+                 'constraints' => [
+                     new IsTrue([
+                         'message' => 'You should agree to our terms.',
+                     ]),
+                 ],
+             ])
+
+             ->add('plainPassword', RepeatedType::class, [
+                 'type' => PasswordType::class,
+                 'mapped' => false,
+                 'constraints' => [
+
+                     new NotBlank([
+                         'message' => 'Veuillez entrer un mot de passe.',
+                     ]),
+
+                     new Length([
+                         'min' => 6,
+                         'minMessage' => 'Votre mot de passe doit au minimum contenir {{ limit }} caractères.',
+                         // Longueur maximale autorisée poru raisons de sécurité
+                         'max' => 4096,
+                     ]),
+
+                     // regex caractére spécial : 
+                     new Assert\Regex([
+                         'pattern' => '/[^A-Za-z0-9]+/',
+                         'message' => 'Vous devez saisir au moins 1 caractére spécial.'
+                         ]),
+
+                     //regex Majuscule : 
+                     new Assert\Regex([
+                         'pattern' => '/[A-Z]+/',
+                         'message' => 'Vous devez saisir au moins 1 Majuscule.'
+                        ]),
+                 ],
+                 'invalid_message' => 'Les champs mot de passe doivent correspondre',
+                 'options' => ['attr' => ['class' => 'password-field']],
+                 'required' => true,
+                 'first_options'  => ['label' => 'Mot de passe','label_attr' => ['class' => 'form-label']],
+                 'second_options' => ['label' => 'Veuillez répéter votre mot de passe','label_attr' => ['class' => 'form-label']],
+                 'label_attr' => ['class' => 'form-label'],
+             ])
+
+             ->add('email', RepeatedType::class,[
+                 'type' => EmailType::class,
+                 'constraints' => [
+                     new Assert\Regex([
+                         'pattern' => '/^\w*[a-zA-Z0-9-_âêîôûäëïöüéèàçÂÊÎÔÛÄËÏÖÜÀÆæÇÉÈŒœÙ]*+@\w*[a-zA-Z0-9-_âêîôûäëïöüéèàçÂÊÎÔÛÄËÏÖÜÀÆæÇÉÈŒœÙ]*+\.\w+$/',
+                         'message' => 'Email invalide'
+                     ])
+                 ],
+                 'invalid_message' => 'Les champs email doivent correspondre.',
+                 'options' => ['attr' => ['class' => 'email-field']],
+                 'required' => true,
+                 'first_options'  => ['label' => 'email','label_attr' => ['class' => 'form-label']],
+                 'second_options' => ['label' => 'Veuillez répéter votre email','label_attr' => ['class' => 'form-label']],
             ])
         ;
     }
