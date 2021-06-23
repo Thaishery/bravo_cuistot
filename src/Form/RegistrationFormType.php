8ab7ac97b9a5453481c8055c233d7789a1e24569
thaishery <gdeb@gdeb.fr>
2021-06-23 11:33:27 +0200

    password probléme

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -75,7 +77,7 @@ use Symfony\Component\Validator\Constraints\File;
                      'required' => false,
                  ])
 
-             // * Mot de passe :
+            //  * Mot de passe :
 
                  ->add('plainPassword', RepeatedType::class, [
                      'type' => PasswordType::class,
