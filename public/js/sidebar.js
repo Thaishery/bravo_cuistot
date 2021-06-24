a8ee2febdb289fb587ea1aa22bb54b406f10ca8f
jordancarmont <biziboy91@gmail.com>
2021-06-24 11:57:59 +0200

    SideBar update

---
+++ public/js/sidebar.js
@@ -0,0 +1,14 @@
+ // ? Feuille de Script de la Side Bar.
+
+     // * Définit la largeur de la " SideBar" de 250px et la marge gauche du contennu de la page de 250px.
+     function openNav() {
+         document.getElementById("mySidebar").style.width = "250px";
+         document.getElementById("main").style.marginLeft = "250px";
+     }
+ 
+   // * Définit la largeur de la " SideBar " à 0 et la marge gauche du contenu de la page à 0.
+     function closeNav() {
+       document.getElementById("mySidebar").style.width = "0";
+       document.getElementById("main").style.marginLeft = "0";
+     }
+    
