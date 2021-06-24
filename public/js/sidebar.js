4dcaa1ef7df443d70313f3e7b6426bc7b49285ff
jordancarmont <biziboy91@gmail.com>
2021-06-24 15:54:51 +0200

    Modif register + footer

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,14 +1,13 @@
  // ? Feuille de Script de la Side Bar.
 
-     // * Définit la largeur de la " SideBar" de 250px et la marge gauche du contennu de la page de 250px.
+   // *** Ouvrir la SideBar  
+
      function openNav() {
-         document.getElementById("mySidebar").style.width = "250px";
-         document.getElementById("main").style.marginLeft = "250px";
-     }
- 
-   // * Définit la largeur de la " SideBar " à 0 et la marge gauche du contenu de la page à 0.
-     function closeNav() {
-       document.getElementById("mySidebar").style.width = "0";
-       document.getElementById("main").style.marginLeft = "0";
+       document.getElementById("myNav").style.width = "100%";
      }
     
+   // *** Ouvrir la SideBar   
+     
+     function closeNav() {
+       document.getElementById("myNav").style.width = "0%";
+     }
