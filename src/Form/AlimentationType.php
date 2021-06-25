fa0c5644de0969ea1e4a23d1a6b6265cfba63b89
thaishery <gdeb@gdeb.fr>
2021-06-26 01:47:50 +0200

    Alimentation => ok, Commentaires => a vérifier, reste a faire

--- src/Form/AlimentationType.php
+++ src/Form/AlimentationType.php
@@ -4,15 +4,29 @@ namespace App\Form;
 
 use App\Entity\Alimentation;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class AlimentationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class,[
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un nom.'
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[a-zA-ZàâäêéèëîïôöùûüÀÂÄÊËÎÏÔÖÙÛÜŒœÇç]/',
+                        'message' => 'Ce champ ne peut contenir que des caractéres alphabétiques, accentuation incluse.'
+                    ])
+                ],
+                'required' => true,
+            ])
         ;
     }
 
