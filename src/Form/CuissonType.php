e4d948a94bed2bfd3d8b4ee385ce3089642f70de
thaishery <gdeb@gdeb.fr>
2021-06-26 17:59:24 +0200

    todo : regex recette

--- src/Form/CuissonType.php
+++ src/Form/CuissonType.php
@@ -6,13 +6,26 @@ use App\Entity\Cuisson;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class CuissonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class,[
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un type de cuisson.'
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[a-zA-ZàâäêéèëîïôöùûüÀÂÄÊËÎÏÔÖÙÛÜŒœÇç]/',
+                        'message' => 'Ce champ ne peut contenir que des caractéres alphabétiques, accentuation incluse.'
+                    ])
+                ],
+                'required' => true,
+            ])
         ;
     }
 
