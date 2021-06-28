68ddc4002eea4759d7827536e14d3604caca86f0
thaishery <gdeb@gdeb.fr>
2021-06-28 03:40:16 +0200

    mise en place service avatarUploarder

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,8 +9,10 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\File;
 
 class UserType extends AbstractType
 {
@@ -43,7 +45,26 @@ class UserType extends AbstractType
                 ])
                 
             
-            ->add('avatar')
+            ->add('avatar',
+            FileType::class, [
+                    'constraints' => [
+                        new File([
+                            'maxSize' => '1024k',
+                            'mimeTypes' => [
+                                'image/jpeg',
+                                'image/bmp',
+                                'image/gif',
+                                'image/png',
+                                'image/svg+xml',
+                                'image/tiff',
+                                'image/webp',
+                                ]
+                        ])
+                            ],
+                            'required'   => false,
+                    'mapped' => false,
+                ]
+             )
             ->add('email', EmailType::class)
         ;
     }
