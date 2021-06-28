f2ed73446157b2882ddf02f2f1910feef29ed6aa
thaishery <gdeb@gdeb.fr>
2021-06-28 03:31:44 +0200

    a

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -22,6 +22,7 @@ class RegistrationController extends AbstractController {
          $form = $this->createForm(RegistrationFormType::class, $user);
          $form->handleRequest($request);
 
+
          if ($form->isSubmitted() && $form->isValid()) {
              // encode the plain password
              $user->setPassword(
