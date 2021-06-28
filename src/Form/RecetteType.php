3e83a5bfc07f94ab7b2d4e77c4a8048f41018a23
thaishery <gdeb@gdeb.fr>
2021-06-28 10:58:31 +0200

    name ok

--- src/Form/RecetteType.php
+++ src/Form/RecetteType.php
@@ -6,13 +6,25 @@ use App\Entity\Recette;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class RecetteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class,[
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un nom de recette.'
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[a-zA-ZàâäêéèëîïôöùûüÀÂÄÊËÎÏÔÖÙÛÜŒœÇç]/',
+                        'message' => 'Ce champ ne peut contenir que des caractéres alphabétiques, accentuation incluse.',
+                    ])
+                ]
+            ])
             ->add('image')
             ->add('temps_preparation')
             ->add('temps_cuisson')
