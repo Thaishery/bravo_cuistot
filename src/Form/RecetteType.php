e60a4f0d92b963acf11ea96f5c766c5c449d07e0
thaishery <gdeb@gdeb.fr>
2021-06-28 11:16:40 +0200

    regex ok, a tester

--- src/Form/RecetteType.php
+++ src/Form/RecetteType.php
@@ -4,10 +4,15 @@ namespace App\Form;
 
 use App\Entity\Recette;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\File;
+use Webmozart\Assert\Assert as AssertAssert;
 
 class RecetteType extends AbstractType
 {
@@ -25,16 +30,76 @@ class RecetteType extends AbstractType
                     ])
                 ]
             ])
-            ->add('image')
-            ->add('temps_preparation')
-            ->add('temps_cuisson')
-            ->add('nb_personnes')
-            ->add('difficulte')
-            ->add('author_id')
-            ->add('cuisson_id')
-            ->add('alimentation_id')
-            ->add('plats_id')
-            ->add('users_fav_id')
+            ->add('image',
+                FileType::class, [
+                    'constraints' => [
+                        new File([
+                            'maxSize' => '1024k',
+                            'mimeTypes' => [
+                                'image/jpeg',
+                                'image/bmp',
+                                'image/gif',
+                                'image/png',
+                                'image/svg+xml',
+                                'image/tiff',
+                                'image/webp',
+                                ]
+                        ])
+                            ],
+                            'required'   => false,
+                    'mapped' => false,
+                ])
+            ->add('temps_preparation', NumberType::class,[
+                'constraints' =>[
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un temps de préparation'
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[0-9]{0,3}/',
+                        'message' => 'Ce champ ne peut contenir que des caractéres numériques (maximum 4) et est exprimé en minutes. max 999 minutes'
+                    ])
+                ]
+            ])
+            ->add('temps_cuisson', NumberType::class,[
+                'constraints' =>[
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un temps de cuisson, mettez 0 si votre recette ne necessite pas de cuisson.'
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[0-9]{0,4}/',
+                        'message' => 'Ce champ ne peut contenir que des caractéres numériques (maximum 3) et est exprimé en minutes. max 9999 minutes'
+                    ])
+                ]
+            ])
+            ->add('nb_personnes', NumberType::class,[
+                'constraints' =>[
+                    new NotBlank([
+                        'message' => 'Veuillez entrer le nombre de pars (pers.).'
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[0-9]{0,2}/',
+                        'message' => 'Ce champ ne peut contenir que des caractéres numériques (maximum 2). max 99 personnes'
+                    ])
+                ]
+            ])
+            ->add('difficulte', NumberType::class,[
+                'constraints' =>[
+                    new NotBlank([
+                        'message' => 'Veuillez entrer une difficulté.'
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/[1-3]{1}/',
+                        'message' => 'Ce champ ne peut varier que de 1 a 3.'
+                    ])
+                ]
+            ])
+            // commentaire temporaire pour tester les regex,
+            // sera récupérer du controller et ajouter, donc pas de sécurisation (champ non exposer)
+            // ->add('author_id')
+            // ->add('cuisson_id')
+            // ->add('alimentation_id')
+            // ->add('plats_id')
+            // ->add('users_fav_id')
         ;
     }
 
