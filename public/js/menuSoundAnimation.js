d41f58b344428480252fa1721f55e0cdea9b2e0c
jordancarmont <biziboy91@gmail.com>
2021-07-05 09:44:27 +0200

    travail sur les vues

---
+++ public/js/menuSoundAnimation.js
@@ -0,0 +1,13 @@
+ // ? Feuille de Script de l'animation On/Off du menu.
+
+   // *** Bouton On
+
+   function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+ 
+// *** Bouton Off
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
