74efacbb087aab07bdfada6da9d4adb60056c2b0
thaishery <gdeb@gdeb.fr>
2021-07-20 16:20:38 +0200

    voir todo dans userIngredientController

--- src/Controller/UserIngredientController.php
+++ src/Controller/UserIngredientController.php
@@ -17,22 +17,25 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class UserIngredientController extends AbstractController
 {
-    // /**
-    //  * @Route("/", name="user_ingredient_index", methods={"GET"})
-    //  */
-    // public function index(IngredientsRepository $ingredientsRepository): Response
-    // {
-    //     return $this->render('user_ingredient/index.html.twig', [
-    //         'ingredients' => $ingredientsRepository->findAll(),
-    //     ]);
-    // }
+    /**
+     * @Route("/", name="user_ingredient_index", methods={"GET"})
+     */
+    public function index(IngredientsRepository $ingredientsRepository): Response
+    {
+        //todo : n'afficher que la liste des ingredient de l'utilisateur actuel. 
+        return $this->render('user_ingredient/index.html.twig', [
+            'ingredients' => $ingredientsRepository->findAll(),
+        ]);
+    }
 
     /**
      * @Route("/new", name="user_ingredient_new", methods={"GET","POST"})
      */
+    // todo : interdiction duplication du nom de l'ingredient (éviter les doublons dans la bdd)
     public function new(Request $request): Response
     {
         $ingredient = new Ingredients();
+        $origin = $request->query->get('recette_id');
         $form = $this->createForm(IngredientsUserType::class, $ingredient);
         $form->handleRequest($request);
         $ingredient->setAuthorId($this->getUser());
@@ -41,6 +44,11 @@ class UserIngredientController extends AbstractController
             $entityManager->persist($ingredient);
             $entityManager->flush();
 
+            if($origin){
+                return $this->redirectToRoute('recette_new_ingredients',[
+                    'id' => $origin,
+                ]);
+            };
             return $this->redirectToRoute('user_ingredient_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -50,35 +58,36 @@ class UserIngredientController extends AbstractController
         ]);
     }
 
-    // /**
-    //  * @Route("/{id}", name="user_ingredient_show", methods={"GET"})
-    //  */
-    // public function show(Ingredients $ingredient): Response
-    // {
-    //     return $this->render('user_ingredient/show.html.twig', [
-    //         'ingredient' => $ingredient,
-    //     ]);
-    // }
+    /**
+     * @Route("/{id}", name="user_ingredient_show", methods={"GET"})
+     */
+    public function show(Ingredients $ingredient): Response
+    {
+        return $this->render('user_ingredient/show.html.twig', [
+            'ingredient' => $ingredient,
+        ]);
+    }
 
-    // /**
-    //  * @Route("/{id}/edit", name="user_ingredient_edit", methods={"GET","POST"})
-    //  */
-    // public function edit(Request $request, Ingredients $ingredient): Response
-    // {
-    //     $form = $this->createForm(Ingredients1Type::class, $ingredient);
-    //     $form->handleRequest($request);
+    /**
+     * @Route("/{id}/edit", name="user_ingredient_edit", methods={"GET","POST"})
+     */
+    // todo : vérification de l'auteur de l'ingredient pour modification. 
+    public function edit(Request $request, Ingredients $ingredient): Response
+    {
+        $form = $this->createForm(IngredientsUserType::class, $ingredient);
+        $form->handleRequest($request);
 
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $this->getDoctrine()->getManager()->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
 
-    //         return $this->redirectToRoute('user_ingredient_index', [], Response::HTTP_SEE_OTHER);
-    //     }
+            return $this->redirectToRoute('user_ingredient_index', [], Response::HTTP_SEE_OTHER);
+        }
 
-    //     return $this->renderForm('user_ingredient/edit.html.twig', [
-    //         'ingredient' => $ingredient,
-    //         'form' => $form,
-    //     ]);
-    // }
+        return $this->renderForm('user_ingredient/edit.html.twig', [
+            'ingredient' => $ingredient,
+            'form' => $form,
+        ]);
+    }
 
     // /**
     //  * @Route("/{id}", name="user_ingredient_delete", methods={"POST"})
