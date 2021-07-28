ff3373c0abe8f87544828ac2d6ecb5c7ee97b404
jordancarmont <biziboy91@gmail.com>
2021-07-28 11:20:20 +0200

    Création page Contact 🐱‍💻

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
-{
-    /**
+class HomeController extends AbstractController {
+     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
-}
+     public function index(): Response {
+         return $this->render('home/index.html.twig', [
+             'controller_name' => 'HomeController',
+         ]);
+     }
+
+     /**
+     * @Route("/contact",name="contact")
+     */
+     public function contact(): Response {
+         return $this->render ('contact/index.html.twig');
+     }
+
+ }
